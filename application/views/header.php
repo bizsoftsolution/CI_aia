bef3a747c35fb0e4dfbff789bdc08d0b89e2c54d
Raj Kumar <rajkumar.bizsoft@gmail.com>
2018-06-27 17:59:51 +0530

    added

--- application/views/header.php
+++ application/views/header.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head>
+<head><!---->
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title><?php echo $page_title;?></title>
 <script type="text/javascript" language="javascript" src="http://jqueryjs.googlecode.com/files/jquery-1.3.2.min.js"></script>
